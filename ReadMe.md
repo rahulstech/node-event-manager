8c75adbbda0c88d57cb7444f1c96be3ce93aec81
rahulstech <rahulstech18@gmail.com>
2025-01-14 17:04:47 +0530

    multer basics

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,19 @@
 ## Learning Express Js
+
+in nodejs 18 and above auto rerun feature is build in. need to run the script in following way
+
+```bsh
+node --watch index.js
+```
+this will keep running the index.js whenever there is any changes only in index.js and index.js runs without throwing any error.
+to rerun the script whenever any file in a directory changes, run the script with the following command
+
+```bsh
+node --watch-path=src index.js
+```
+the command watch changes in src directory,and if there is any change then it rerun the index.js utill index.js
+runs without throwing any error. i can watch multiple path just by adding more '--watch-path=/path/to/other/directory'. before node 18 it is achived via nodemon package; but node 18 onwards this feature is builtin. so, no need for nodemon anymore.
+
+### Projects:
+
+1. [**EventManager**](/eventmanager/ReadMe.md)
---
+++ multer-tutorial/app.js
@@ -0,0 +1,35 @@
+
+/**
+ * learnings:
+ * 1. __dirname and __filename are available when commonjs module type is set in package.json. in ES module these are undefined
+ * 
+ * 2. process.cwd() vs __dirname: __dirname returns the directory where the current script is saved. process.cwd() returns the directory
+ *    from which the current script is run. for example: let the script path /home/user/node-examples/app/index.js. currently i am in /home/user/node-example/
+ *    and i run script from here with 'node app/index.js". then __dirname = /home/user/node-example/app/ and process.cwd() = /home/user/node-example.
+ *    NOTE: path.resolve('media') will resolve it using process.cwd() but path.resolve(__dirname, 'media') with resolve it using __dirname
+ * 
+ * 
+ * 
+ * 
+ */
+const express = require('express')
+
+const multer = require('multer')
+
+const path = require('node:path')
+
+const multipartmw = multer({ dest: path.resolve(__dirname, './uploads')})
+
+const server = express()
+
+server.post('/upload', multipartmw.single('image'), (req, res) => {
+    const { body, file } = req
+
+    console.log('body: ', body)
+    console.log('file: ', file)
+
+    res.send('success\n')
+})
+
+const port = 3000
+server.listen(port, () => console.log(`server listening on port ${port}`))
