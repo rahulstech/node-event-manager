848b721d22789dc6750129504e87f9ac0e72b73b
rahulstech <rahulstech18@gmail.com>
2025-01-16 20:44:56 +0530

    test complex promise

--- jest-tutorial/async.test.js
+++ jest-tutorial/async.test.js
@@ -14,11 +14,66 @@ test('the data is peanut butter', async () => {
     expect(data).toBe('peanut butter');
 });
 
-// test('the fetch fails with an error', async () => {
-//     expect.assertions(1);
-//     try {
-//         await fetchData();
-//     } catch (e) {
-//         expect(e).toMatch('error');
-//     }
-// });
+/**
+ * Test if an async function throws error
+ */
+
+async function doTaskAsyncAndThrow() {
+    throw new Error('this is an error')
+}
+
+// First Approach:
+
+test('doTaskAsyncAndThrow', async () => {
+    // use async callback for test
+    await expect(doTaskAsyncAndThrow()).rejects.toThrow()
+})
+
+// Second Approach:
+
+test('doTaskAsyncAndThrow: using return', () => {
+    // use 'return' to return from test callback
+    return expect(doTaskAsyncAndThrow()).rejects.toThrow()
+}) 
+
+/**
+ * Test an async function that throws for known cases and does not throws for other cases.
+ * The following function throws errow when the argument is not event number. if the number is
+ * even it returns string 'even'. i will test the two cases for the function. 
+ * first: when i pass 1, a odd number, then i expect it to thow an error
+ * second: when i pass 2, a event number, then i expect the return values is 'even'
+ */
+
+async function doTaskAsyncMayThrow(x) {
+    if (x%2 == 0) {
+        return 'even'
+    }
+    throw new Error(`${x} is not an event number`)
+}
+
+// METHOD 1: test using helper function. this approach works but there is straight forward method.
+// see METHOD 2
+
+const createRunTask = async (x) => {
+    const v = await doTaskAsyncMayThrow(x)
+    expect(v).toEqual('even')
+}
+
+test('doTaskAsyncButDonotThrow: arg 1', () => {
+    return expect(createRunTask(1)).rejects.toThrow()
+})
+
+test('doTaskAsyncButDonotThrow: arg 2', () => {
+    return expect(createRunTask(2)).resolves.not.toThrow()
+})
+
+// METHOD 2: this approach is straight forward and recomended way to test this cases
+
+test('doTaskAsyncButDonotThrow: arg 4 with async test function', () => {
+    // use 'resolves' of expect() when test function Promise resolves successfully
+    return expect(doTaskAsyncMayThrow(2)).resolves.toEqual('even') 
+})
+
+test('doTaskAsyncButDonotThrow: arg 4 with async test function', () => {
+    return expect(doTaskAsyncMayThrow(1)).rejects.toThrow()
+})
